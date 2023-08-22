5c6f0bcec2f52ac8dcb5bc1661974be994ac57d6
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2023-08-22 23:29:31 +0300

    Updating ChallengeBox
    - Modified ChallengeBox by adding text 'no challenges !' text if the array of challenges is empty;

--- src/components/home/challengeBox.jsx
+++ src/components/home/challengeBox.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function ChallengeBox({ challenges2 = null }) {
-  const challenges = [
+    const challenges = [
         { title: 'Challenge 1', status: 'Finished' },
         { title: 'Challenge 2', status: 'On Going' },
         { title: 'Challenge 3', status: 'Waiting' },
@@ -11,21 +11,25 @@ function ChallengeBox({ challenges2 = null }) {
         { title: 'Challenge 1', status: 'Finished' },
         { title: 'Challenge 2', status: 'On Going' },
         { title: 'Challenge 1', status: 'Finished' },
-  ];
+    ];
 
-  return (
-    <div className="button-container h-fit p-4">
-        <p className="self-start mb-6 text-2xl font-semibold">Active Challenges <span className='px-2 ml-2 rounded-xl text-lg bg-primary text-white'>{challenges.length}</span></p>
-        <ul className="flex flex-col w-full h-full gap-6">
-        {challenges.map((challenge, index) => (
-            <li key={index} className="flex flex-row justify-between items-center">
-                <p className='font-semibold'>{challenge.title}</p>
-                <span className={`px-3 py-1 rounded-2xl font-semibold ${getStatusColor(challenge.status)}`}>{challenge.status}</span>
-            </li>
-        ))}
-        </ul>
-    </div>
-  );
+    return (
+        <div className="button-container h-fit p-4">
+            <p className="self-start mb-6 text-2xl font-semibold">Active Challenges <span className='px-2 ml-2 rounded-xl text-lg bg-primary text-white'>{challenges.length}</span></p>
+            <ul className="flex flex-col w-full h-full gap-6">
+            { challenges.length === 0 ?
+                <p>No challenges !</p>
+            :
+                challenges.map((challenge, index) => (
+                    <li key={index} className="flex flex-row justify-between items-center">
+                        <p className='font-semibold'>{challenge.title}</p>
+                        <span className={`px-3 py-1 rounded-2xl font-semibold ${getStatusColor(challenge.status)}`}>{challenge.status}</span>
+                    </li>
+                ))
+            }
+            </ul>
+        </div>
+    );
 };
 
 function getStatusColor(status) {
