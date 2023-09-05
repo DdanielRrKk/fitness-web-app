2d61142b0eb4056d89fed12d4484a4e58e2493c4
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2023-09-05 20:21:19 +0300

    RootPage sidebar UI fix
    - Modified RootPage by making the sidebar be more compatible with the other pages and removing unnecessary 'div' elements;

--- src/pages/rootPage.jsx
+++ src/pages/rootPage.jsx
@@ -14,67 +14,63 @@ function RootPage() {
     const handleIsActiveNavLink = ({ isActive }) => isActive ? "navbar-button-focused block" : "navbar-button block";
 
     return isAuthenticated ?
-        <div className='flex w-full h-screen'>
-            <div className='w-32'>
-                <nav className='flex flex-col w-32 h-full justify-between fixed p-4 bg-primary'>
-                    <div>
-                        <img 
-                            className='mb-4'
-                            src={Logo} 
-                            alt="mtp-logo-icon" />
+        <div className='flex w-full h-full'>
+            <nav className='flex flex-col w-32 h-full justify-between fixed p-4 bg-primary'>
+                <ul className="flex flex-col gap-4">
+                    <img 
+                        className='mb-4'
+                        src={Logo} 
+                        alt="mtp-logo-icon" />
 
-                        <ul className="flex flex-col gap-4">
-                            <li>
-                                <NavLink 
-                                    to='home'
-                                    className={handleIsActiveNavLink}>
-                                        Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    to='meal'
-                                    className={handleIsActiveNavLink}>
-                                        Meal
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    to='workout'
-                                    className={handleIsActiveNavLink}>
-                                        Workout
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    to='learn'
-                                    className={handleIsActiveNavLink}>
-                                        Learn
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
+                    <li>
+                        <NavLink 
+                            to='home'
+                            className={handleIsActiveNavLink}>
+                                Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='meal'
+                            className={handleIsActiveNavLink}>
+                                Meal
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='workout'
+                            className={handleIsActiveNavLink}>
+                                Workout
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='learn'
+                            className={handleIsActiveNavLink}>
+                                Learn
+                        </NavLink>
+                    </li>
+                </ul>
 
-                    <ul className='flex flex-col gap-4'>
-                        <li>
-                            <NavLink 
-                                to='help'
-                                className={handleIsActiveNavLink}>
-                                    Help
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='settings'
-                                className={handleIsActiveNavLink}>
-                                    Settings
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+                <ul className='flex flex-col gap-4'>
+                    <li>
+                        <NavLink 
+                            to='help'
+                            className={handleIsActiveNavLink}>
+                                Help
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='settings'
+                            className={handleIsActiveNavLink}>
+                                Settings
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-            <main className='flex flex-col w-full items-center'>
+            <main className='w-full flex flex-col items-center ml-32'>
                 <Outlet />
             </main>
         </div>
